e35d353331200c8c1a86ff35262c190b64f81eae
José Eduardo <66597004+jemedeiros@users.noreply.github.com>
2020-09-10 15:41:14 +0200

    s

--- js/map.js
+++ js/map.js
@@ -38,7 +38,7 @@ async function get_shapes() {
         }
         counties_shp = L.geoJson(data, {style: style});
         shp_uk_lba_eia_id = counties_shp.addTo(mymap);
-        shp_uk_lba_eia_id.on('loading', loadingHandler);
+        shp_uk_lba_eia_id.on("loading", loadingHandler);
         shp_uk_lba_eia_id.on('load', loadHandler);
         
     } 
